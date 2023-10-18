a560ef8681d630c09fc0c2db4329ce551c9fe2df
tuiza <luiza8.marlene@gmail.com>
2023-10-18 14:35:21 -0300

    feat: fazendo o cache do elemento do dom

--- app/dist/js/controllers/negociacao-controller.js
+++ app/dist/js/controllers/negociacao-controller.js
@@ -4,6 +4,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+import { domInjector } from '../decorators/dom-injector.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
@@ -13,18 +14,14 @@ import { NegociacoesView } from '../views/negociacoes-view.js';
 export class NegociacaoController {
     constructor() {
         this.negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView('#negociacoesView');
-        this.mensagemView = new MensagemView('#mensagemView');
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView = new NegociacoesView("#negociacoesView");
+        this.mensagemView = new MensagemView("#mensagemView");
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
         const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         if (!this.ehDiaUtil(negociacao.data)) {
-            this.mensagemView
-                .update('Apenas negociações em dias úteis são aceitas');
+            this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
             return;
         }
         this.negociacoes.adiciona(negociacao);
@@ -32,20 +29,29 @@ export class NegociacaoController {
         this.atualizaView();
     }
     ehDiaUtil(data) {
-        return data.getDay() > DiasDaSemana.DOMINGO
-            && data.getDay() < DiasDaSemana.SABADO;
+        return (data.getDay() > DiasDaSemana.DOMINGO &&
+            data.getDay() < DiasDaSemana.SABADO);
     }
     limparFormulario() {
-        this.inputData.value = '';
-        this.inputQuantidade.value = '';
-        this.inputValor.value = '';
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
         this.inputData.focus();
     }
     atualizaView() {
         this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação adicionada com sucesso');
+        this.mensagemView.update("Negociação adicionada com sucesso");
     }
 }
+__decorate([
+    domInjector("#data")
+], NegociacaoController.prototype, "inputData", void 0);
+__decorate([
+    domInjector("#quantidade")
+], NegociacaoController.prototype, "inputQuantidade", void 0);
+__decorate([
+    domInjector("#valor")
+], NegociacaoController.prototype, "inputValor", void 0);
 __decorate([
     logarTempoDeExecucao(true)
 ], NegociacaoController.prototype, "adiciona", null);
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,24 +1,24 @@
 export function domInjector(seletor: string) {
   // decoretor de propriedade
-    return function (target: any, propertyKey: string) {
-        console.log(`Modificando protype ${target.contructor.name} e
+  return function (target: any, propertyKey: string) {
+    console.log(`Modificando protype ${target.constructor.name} e
             adicionando getter para a propriedade ${propertyKey}
-        ` )
-        const getter = function () {
-            const elemento = document.querySelector(seletor)
-            console.log(`Busacando elemento DOM com o seletor ${seletor} para injetar em ${propertyKey}`)
-            return elemento
-        }
+        `);
 
-        Object.defineProperty(
-            target,
-            propertyKey,
-            {
-                get: getter
-            }
-        )
+    let elemento: HTMLElement;
+    const getter = function () {
+      if (!elemento) {
+        elemento = document.querySelector(seletor) as HTMLElement;
+        console.log(
+          `Busacando elemento DOM com o seletor ${seletor} para injetar em ${propertyKey}`
+        );
+      }
+      return elemento;
+    };
 
-
+    Object.defineProperty(target, propertyKey, {
+      get: getter,
+    });
   };
 }
 
